364e5e32b4d8966ad96daccb4b27fee1cf56bb53
showmanlee <hoon12560@naver.com>
2020-09-22 00:01:20 +0900

    20.09.21

---
+++ BOJ/15637.py
@@ -0,0 +1,8 @@
+# Lotto
+# https://www.acmicpc.net/problem/15637
+
+lotto = [103, 88, 91, 99, 101, 86, 96, 100, 71, 91, 96, 91, 98, 102, 96, 85, 102, 97, 91, 112, 85, 79, 86, 91, 97, 98, 110, 80, 88, 80, 95, 83, 93, 109, 88, 91, 103, 90, 95, 113, 81, 86, 97, 93, 93]
+n = int(input())
+print(lotto[n-1])
+
+# https://dhlottery.co.kr/gameResult.do?method=statIndex 에서 번호별 당첨 현황을 볼 수 있음
