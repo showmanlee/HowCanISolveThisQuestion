975612dd034f336da036548e8ed142372d338565
showmanlee <hoon12560@naver.com>
2021-01-01 08:54:33 +0900

    21.01.01

---
+++ BOJ/11282.py
@@ -0,0 +1,7 @@
+# 한글 2
+# https://www.acmicpc.net/problem/11282
+
+c = input()
+print(ord(c) - ord('가') + 1) 
+
+# 한글 조합식 코드 원리
---
+++ BOJ/2420.py
@@ -0,0 +1,7 @@
+# 사파리월드
+# https://www.acmicpc.net/problem/2420
+
+a, b = list(map(int, input().split()))
+print(abs(a - b))
+
+# 두 수의 차의 절댓값 구하기
