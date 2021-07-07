fc28601455dea85aa2c541d39c6c64dd0b88aba4
showmanlee <hoon12560@naver.com>
2021-07-08 08:28:29 +0900

    21.07.08

---
+++ BOJ/22193.py
@@ -0,0 +1,11 @@
+# Multiply
+# https://www.acmicpc.net/problem/22193
+
+n, m = list(map(int, input().split()))
+a = int(input())
+b = int(input())
+print(a * b)
+
+# n자리와 m자리 정수 곱하기
+# python에서는 쉬움
+# c에서는 어떻게 할까 - 문자열 기반으로 곱해나가기?
