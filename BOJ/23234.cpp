7bcbe3c126f8c45f327b38362aca6e26ba12c4ae
showmanlee <hoon12560@naver.com>
2021-10-15 07:39:53 +0900

    21.10.15

---
+++ BOJ/23234.cpp
@@ -0,0 +1,17 @@
+// The World Responds
+// https://www.acmicpc.net/problem/23234
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "The world says hello!\n";
+}
+
+// "The world says hello!" 출력하기
+
+// "Hello, world!"의 엘레강스한 버전
