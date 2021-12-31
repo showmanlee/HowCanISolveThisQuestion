5357d1e81d2fa3878880a2c3ed2fac618ff41317
showmanlee <hoon12560@naver.com>
2021-12-31 09:18:31 +0900

    21.12.31

---
+++ BOJ/23804.cpp
@@ -0,0 +1,26 @@
+// 골뱅이 찍기 - ㄷ
+// https://www.acmicpc.net/problem/23804
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n * 5; i++) {
+		for (int j = 0; j < n * 5; j++) {
+			if (i / n == 0 || i / n == 4 || j / n == 0) {
+				cout << '@';
+			}
+		}
+		cout << '\n';
+	}
+}
+
+// 획 두께가 n인 5*n 크기의 ㄷ 그리기
+
+// 저번에 ㄱ 찍었을 때처럼 만들기
