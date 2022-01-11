66af9b243abc58c49a7e44463243509788c44014
showmanlee <hoon12560@naver.com>
2022-01-12 08:46:26 +0900

    22.01.12

---
+++ BOJ/24000/24078.cpp
@@ -0,0 +1,19 @@
+// 余り (Remainder)
+// https://www.acmicpc.net/problem/24078
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	cout << (n % 21) << '\n';
+}
+
+// n을 21로 나눈 나머지 출력하기
+
+// 말 그대로...
