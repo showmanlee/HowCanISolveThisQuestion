f7babe4b361c6ac9d3df2d60622bc17940b9b229
showmanlee <hoon12560@naver.com>
2022-01-13 07:56:44 +0900

    22.01.13

---
+++ BOJ/24000/24082.cpp
@@ -0,0 +1,19 @@
+// 立方体 (Cube)
+// https://www.acmicpc.net/problem/24082
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	cout << (n * n * n) << '\n';
+}
+
+// n을 세제곱한 결과 구하기
+
+// 말 그대로...
