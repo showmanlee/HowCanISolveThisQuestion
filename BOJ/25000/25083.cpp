371fcd4204b0077b2b913f663fa7b393ee99f668
showmanlee <hoon12560@naver.com>
2022-05-01 08:45:53 +0900

    22.05.01

---
+++ BOJ/25000/25083.cpp
@@ -0,0 +1,23 @@
+// 새싹
+// https://www.acmicpc.net/problem/25083
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+   cin.tie(NULL);
+   ios_base::sync_with_stdio(false);
+
+   cout << "         ,r'\"7" << '\n';
+   cout << "r`-_   ,'  ,/" << '\n';
+   cout << " \\. \". L_r'" << '\n';
+   cout << "   `~\\/" << '\n';
+   cout << "      |" << '\n';
+   cout << "      |" << '\n';
+}
+
+// 새싹 출력하기
+
+// 또 하나의 브론즈 5 그대로 출력하기 문제
+// 숨어있는 "과 \을 이스케이프로 처리해야 함에 주의
