61dc7137b6691d7890fcf821297777931d4e5e11
showmanlee <hoon12560@naver.com>
2022-06-02 06:11:12 +0900

    22.06.02

---
+++ BOJ/8000/8545.cpp
@@ -0,0 +1,19 @@
+// Zadanie próbne
+// https://www.acmicpc.net/problem/8545
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	char a, b, c;
+	cin >> a >> b >> c;
+	cout << c << b << a << '\n';
+}
+
+// 주어진 3글자를 거꾸로 출력하기
+
+// 3글자만 주어지므로 문자열을 쓸 필요도 없음
