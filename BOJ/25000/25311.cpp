90f0da59178f5b0b33d5f5325b988edf036fa517
showmanlee <hoon12560@naver.com>
2022-07-05 08:17:23 +0900

    22.07.05

---
+++ BOJ/25000/25311.cpp
@@ -0,0 +1,19 @@
+// UCPC에서 가장 쉬운 문제 번호는?
+// https://www.acmicpc.net/problem/25311
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    cout << "A\n";
+}
+
+// 연도가 주어지면 그 연도의 UCPC에서 가장 쉬운 문제 번호 출력하기
+
+// 2022년이 PA처럼 보일 수도 있으나, 조건에 '알파벳 한 글자'로 출력하라고 했기에 무조건 A임
