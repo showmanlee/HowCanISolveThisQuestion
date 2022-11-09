fccedb34faba79d4c40e5cc44e409c2c83928dd0
showmanlee <hoon12560@naver.com>
2022-11-10 06:37:00 +0900

    22.11.10

---
+++ BOJ/3000/3765.cpp
@@ -0,0 +1,21 @@
+// Celebrity jeopardy
+// https://www.acmicpc.net/problem/3765
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string s;
+	while (getline(cin, s)) {
+		cout << s << '\n';
+	}
+}
+
+// 주어진 수식을 공백을 살려 가장 간단한 형태로 출력하기
+
+// 그러니까 그대로 출력하라는 말
