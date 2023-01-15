7c7f2d0bc73115ea86176f7967e1c216a2e4e5ea
showmanlee <hoon12560@naver.com>
2023-01-15 10:19:00 +0900

    23.01.15

---
+++ BOJ/10000/10189.cpp
@@ -0,0 +1,21 @@
+// Hook
+// https://www.acmicpc.net/problem/10189
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "#  # #### #### #  #\n";
+	cout << "#### #  # #  # # # \n";
+	cout << "#### #  # #  # # # \n";
+	cout << "#  # #### #### #  #\n";
+}
+ 
+// #으로 만든 Hook 출력하기
+
+// 네 출력할게요
