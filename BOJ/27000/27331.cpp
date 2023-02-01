5e614d65a14bd528aaf5e135b7f3875f184bf873
showmanlee <hoon12560@naver.com>
2023-02-02 06:32:05 +0900

    23.02.02

---
+++ BOJ/27000/27331.cpp
@@ -0,0 +1,19 @@
+// 2 桁の整数 (Two-digit Integer)
+// https://www.acmicpc.net/problem/27331
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a << b << '\n';
+}
+
+// 십의자리 숫자와 일의자리 숫자가 주어졌을 때, 두자리 정수 출력하기
+
+// 네
