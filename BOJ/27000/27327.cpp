3059678e53449c7db763495e7f17ce7b55c3ea64
showmanlee <hoon12560@naver.com>
2023-02-25 10:05:13 +0900

    23.02.25

---
+++ BOJ/27000/27327.cpp
@@ -0,0 +1,19 @@
+// 時間 (Hour
+// https://www.acmicpc.net/problem/27327
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	cout << (n * 24) << '\n';
+}
+
+// n일을 시간으로 변환하기
+
+// 하루는 24시간
