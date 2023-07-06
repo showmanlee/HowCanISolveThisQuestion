3520d1cea92ccbfcf40743b705c3885af3868f70
showmanlee <hoon12560@naver.com>
2023-07-07 06:56:42 +0900

    23.07.07

--- BOJ/10000/10815.cpp
+++ BOJ/10000/10815.cpp
@@ -3,29 +3,32 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include <algorithm>
 using namespace std;
 
 int main(void) {
 	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
 
 	int n;
 	cin >> n;
-	vector<int> board(n);
-	for (int i = 0; i < n; i++)
-		cin >> board[i];
-	sort(board.begin(), board.end());
+	map<int, bool> v;
+	for (int i = 0; i < n; i++) {
+		int p;
+		cin >> p;
+		v[p] = true;
+	}
 	int m;
 	cin >> m;
 	for (int i = 0; i < m; i++) {
 		int p;
 		cin >> p;
-		cout << (*lower_bound(board.begin(), board.end(), p) == p) << ' ';
+		cout << v[p] << ' ';
 	}
+	cout << '\n';
 }
 
-// 현재 가지고 있는 숫자카드들 중 선택한 숫자들이 있나요?
-// 숫자카드 범위가 +-1천만, 카드의 수는 50만, 쿼리의 수도 50만
-// 대놓고 이분탐색 쓰라고 만든 문제
-// 받은 숫자카드들을 정렬한 후, lower_bound를 돌려 해당 위치의 수가 찾는 숫자라면 1 표시
+// 현재 가지고 있는 정수 카드들과 다른 정수들이 주어졌을 때, 주어진 정수들이 적힌 카드가 있는지 판정하기
+
+// 카드는 map으로 기록하고, 주어진 정수들을 받으며 map에 true로 찍혀있는지 출력하기
