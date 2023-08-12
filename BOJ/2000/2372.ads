ba06fe256611694704e6fb5b6856cceb82545048
showmanlee <hoon12560@naver.com>
2023-08-12 10:03:25 +0900

    23.08.12

---
+++ BOJ/2000/2372.ads
@@ -0,0 +1,18 @@
+-- Livestock Count
+-- https://www.acmicpc.net/problem/2372
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure program is
+begin
+  Put_Line("Animal      Count");
+  Put_Line("-----------------");
+  Put_Line("Chickens      100");
+  Put_Line("Clydesdales     5");
+  Put_Line("Cows           40");
+  Put_Line("Goats          22");
+  Put_Line("Steers          2");
+end program;
+
+-- 주어진 대로 출력하기
+-- 이 언어를 쓸 일이 없고, 구조는 똑같을 수밖에 없음
