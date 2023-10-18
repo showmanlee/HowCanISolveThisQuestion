d6ff0585bd13c0731204d1b3b27272db3f887b58
showmanlee <hoon12560@naver.com>
2023-10-19 08:35:05 +0900

    23.10.19

---
+++ BOJ/27000/27465.cpp
@@ -0,0 +1,20 @@
+// 소수가 아닌 수
+// https://www.acmicpc.net/problem/27465
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    cout << 1000000000 << '\n';
+}
+
+// 주어진 수 이상의 소수가 아닌 정수 출력하기
+
+// 최댓값은 10^9 - 아무거나 출력하면 되므로 최대치를 출력하면 되지
+// 10^9는 소수가 아니므로 그걸 출력해주면 됨
