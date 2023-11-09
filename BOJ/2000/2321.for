a709206dc88e084c02a8be9ffc5bdc7c76faa865
showmanlee <hoon12560@naver.com>
2023-11-10 08:31:31 +0900

    23.11.10

---
+++ BOJ/2000/2321.for
@@ -0,0 +1,14 @@
+C Crowling
+C https://www.acmicpc.net/problem/2321
+
+program main
+  print '(a)', ",~"
+  print '(a)', "( 9> //)"
+  print '(a)', " )(_///"
+  print '(a)', "( \_>/"
+  print '(a)', " \__/"
+  print '(a)', "  ^^"
+end program main
+
+C 포트란으로 문자열 출력하기
+C 대충 구조는 이렇답니다... 기억할 필요는 없을 수도 있겠네요...
