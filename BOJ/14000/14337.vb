84dba02d794b9b563abf2b0511df30cc0b567c79
showmanlee <hoon12560@naver.com>
2023-11-12 09:42:21 +0900

    23.11.12

---
+++ BOJ/14000/14337.vb
@@ -0,0 +1,17 @@
+' Helicopter
+' https://www.acmicpc.net/problem/14337
+
+Module Helicopter
+    Sub Main()
+        Console.WriteLine("      ===================")
+        Console.WriteLine("          ____||___")
+        Console.WriteLine("\ /      /       []\")
+        Console.WriteLine(" X=======           \__")
+        Console.WriteLine("/ \      \____________|")
+        Console.WriteLine("            ||  ||")
+        Console.WriteLine("         \-----------/")
+    End Sub
+End Module
+
+' Visual Basic으로 주어진 문자열 출력하기
+' 하세요, .vb 파일로다가
