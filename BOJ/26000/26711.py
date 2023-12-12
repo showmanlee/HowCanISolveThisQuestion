8607181138e10d38d42d70321f1f02ebe3e1a980
showmanlee <hoon12560@naver.com>
2023-12-13 08:03:33 +0900

    23.12.13

---
+++ BOJ/26000/26711.py
@@ -0,0 +1,10 @@
+# A+B
+# https://www.acmicpc.net/problem/26711
+
+a = int(input())
+b = int(input())
+print(int(a) + int(b))
+
+# a + b 출력하기
+
+# 바쁜 상황에서 스트릭 유지용 문제 - 5000자리까지 들어올 수 있으므로 python으로...
