08c6baf536f77f6da120ae55443f41828234bfc5
showmanlee <hoon12560@naver.com>
2024-06-28 09:01:28 +0900

    24.06.28

---
+++ BOJ/30000/30328.cpp
@@ -0,0 +1,18 @@
+// Java Warriors
+// https://www.acmicpc.net/problem/30328
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    cout << n * 4000 << '\n';
+}
+
+// n * 4000 출력하기
