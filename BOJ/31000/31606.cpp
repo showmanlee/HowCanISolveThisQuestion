7dbab736eaad1def063f4d1e7a5cb7b51004176b
showmanlee <hoon12560@naver.com>
2024-07-08 09:13:11 +0900

    24.07.08

---
+++ BOJ/31000/31606.cpp
@@ -0,0 +1,17 @@
+// 果物 (Fruit)
+// https://www.acmicpc.net/problem/31606
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b + 3) << '\n';
+}
+
+// a + b + 3 구하기
