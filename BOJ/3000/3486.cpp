dc56b014e8a624c6e049b3b32074d84e198d9489
showmanlee <hoon12560@naver.com>
2025-02-15 07:19:36 +0900

    25.02.15

---
+++ BOJ/3000/3486.cpp
@@ -0,0 +1,28 @@
+// Adding Reversed Numbers
+// https://www.acmicpc.net/problem/3486
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int tt;
+	cin >> tt;
+	for (int t = 0; t < tt; t++) {
+		string a, b;
+		cin >> a >> b;
+		reverse(a.begin(), a.end());
+		reverse(b.begin(), b.end());
+		int p = stoi(a) + stoi(b);
+		string r = to_string(p);
+		reverse(r.begin(), r.end());
+		int res = stoi(r);
+		cout << res << '\n';
+	}
+}
+
+// 주어진 두 정수의 숫자 순서를 뒤집어서 더한 후 그 결과의 숫자 순서를 다시 뒤집은 결과 출력하기
