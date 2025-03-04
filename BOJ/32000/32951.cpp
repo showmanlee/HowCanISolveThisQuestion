56758152d467b649c2927837812ebea404a4c060
showmanlee <hoon12560@naver.com>
2025-03-05 07:08:26 +0900

    25.03.05

---
+++ BOJ/32000/32951.cpp
@@ -0,0 +1,18 @@
+// AI 선도대학
+// https://www.acmicpc.net/problem/32951
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	cout << (n - 2024) << '\n';
+}
+
+// n - 2024 구하기
