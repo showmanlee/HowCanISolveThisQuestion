aaddc4fc617e9ac9aa7140ad9ca64d1cebe3dd71
showmanlee <hoon12560@naver.com>
2025-04-06 09:36:25 +0900

    25.04.06

---
+++ BOJ/33000/33701.cpp
@@ -0,0 +1,16 @@
+// 새천년관
+// https://www.acmicpc.net/problem/33701
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "nkugwan" << '\n';
+}
+
+// 주어진 대로 문자열 만들기
