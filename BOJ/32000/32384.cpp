681abc716d986e29bcd15f990033876a206932e4
showmanlee <hoon12560@naver.com>
2025-10-09 09:49:38 +0900

    25.10.09

---
+++ BOJ/32000/32384.cpp
@@ -0,0 +1,21 @@
+// 사랑은 고려대입니다
+// https://www.acmicpc.net/problem/32384
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "LoveisKoreaUniversity ";
+	}
+	cout << '\n';
+}
+
+// LoveisKoreaUniversity n번 출력하기
