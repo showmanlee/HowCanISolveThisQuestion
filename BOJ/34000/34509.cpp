b9b59109124ccf8498974002f628eca73493c4e0
showmanlee <hoon12560@naver.com>
2025-10-14 08:40:40 +0900

    25.10.14

---
+++ BOJ/34000/34509.cpp
@@ -0,0 +1,15 @@
+// 2, 4, 6, 8
+// https://www.acmicpc.net/problem/34509
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cout << "42" << '\n';
+}
+
+// 2자리수고 뒤집으면 4의 배수가 되면서 각 자릿수의 합이 6의 배수고 8이 없는 수 구하기
