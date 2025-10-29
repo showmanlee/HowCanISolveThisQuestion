725a9f8b46d0f7bd642048556903ffb80085f504
showmanlee <hoon12560@naver.com>
2025-10-30 07:29:33 +0900

    25.10.30

---
+++ BOJ/34000/34446.cpp
@@ -0,0 +1,17 @@
+// E-Days Ore Cart Pull
+// https://www.acmicpc.net/problem/34446
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, m, t;
+	cin >> n >> m >> t;
+	cout << (n * 2) << '\n';
+}
+
+// n * 2 출력하기
