f344d5e320ce3628e2361e5dab47b3569b01ff3d
showmanlee <hoon12560@naver.com>
2025-11-17 08:51:20 +0900

    25.11.17

---
+++ BOJ/34000/34721.cpp
@@ -0,0 +1,20 @@
+// 역사를 걸으면 동국이 보이고
+// https://www.acmicpc.net/problem/34721
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "I love DGU\n";
+	}
+}
+
+// 문자열 n번 출력하기
