cd950a5a23f21bf272d2e20f7b71a6b7b6bc2377
showmanlee <hoon12560@naver.com>
2025-11-26 07:32:59 +0900

    25.11.26

---
+++ BOJ/34000/34667.cpp
@@ -0,0 +1,17 @@
+// 가희와 철도역
+// https://www.acmicpc.net/problem/34667
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string s, t;
+	cin >> s >> t;
+	cout << s[0] << s << '\n';
+}
+
+// 문자열 s, t가 주어질 때, 문제의 조건을 만족하는 문자열 v 구하기
