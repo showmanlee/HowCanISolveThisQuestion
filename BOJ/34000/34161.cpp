eb1eaf7628e01644f9dc04166eef38bcc68b7f21
showmanlee <hoon12560@naver.com>
2025-12-22 07:36:07 +0900

    25.12.22

---
+++ BOJ/34000/34161.cpp
@@ -0,0 +1,22 @@
+// OO0OO
+// https://www.acmicpc.net/problem/34161
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	for (int i = 0; i < 3; i++) {
+		string s;
+		cin >> s;
+	}
+	for (int i = 0; i < 10000; i++) {
+		cout << -1 << '\n';
+	}
+}
+
+// 고래는 알을 낳을 수 없음을 출력하기
