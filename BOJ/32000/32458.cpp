b91a1772133d45cb308a9c4b38d83e368510fc8f
showmanlee <hoon12560@naver.com>
2026-01-20 08:01:20 +0900

    26.01.20

---
+++ BOJ/32000/32458.cpp
@@ -0,0 +1,17 @@
+// Just Round Down
+// https://www.acmicpc.net/problem/32458
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	double d;
+	cin >> d;
+	cout << (int)d << '\n';
+}
+
+// 주어신 실수 내림하기
